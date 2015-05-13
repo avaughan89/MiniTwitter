90e518eb13310a706adc29d7e3676a2f49c5a19a
Andrew Vaughan <asdvaughan@gmail.com>
2015-05-13 16:00:01 -0700

    Added contact page and test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,34 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "MiniTwitter | "
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home"
+    assert_select "title", "#{@base_title}Home"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help"
+    assert_select "title", "#{@base_title}Help"
 
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About"
+    assert_select "title", "#{@base_title}About"
+  end
+
+  test 'should get contact' do
+    get :contact
+    assert_response :success
+    assert_select "title", "#{@base_title}Contact"
   end
 
 end
